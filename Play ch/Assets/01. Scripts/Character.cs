f991b75820ef4512603f9ac5c1f7d22efeca6068
JJongCha <mnbvcxz10@naver.com>
2019-02-16 13:31:13 +0900

    Assetddgo

---
+++ Play ch/Assets/01. Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    //SerializeField 
+    //CharacterControllor
+    [SerializeField] Animator _animator;    
+
+    void Start()
+    {
+        IdleState();
+    }
+
+    void Update()
+    {
+    }
+
+        //상태 
+    void IdleState()
+    {
+        string trggerName = "idle1";
+        int randValue = Random.Range(1, 6);
+        trggerName = "idle" + randValue;
+        _animator.SetTrigger(trggerName);
+        
+    }
+}
