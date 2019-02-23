da46aef12c0ffe2170a40fcbb6ba3dc0cc7c10e0
JJongCha <mnbvcxz10@naver.com>
2019-02-23 13:33:22 +0900

    수정2

---
+++ Play ch/Assets/01. Scripts/AnimationController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Animation Event
+
+    System.Action _endEvent = null;
+
+    public void PLay(string triggerName, System.Action endEvent)
+    {
+        _animator.SetTrigger(triggerName);
+        _endEvent = endEvent;
+    }
+
+
+    public void AddEndEvent(System.Action endEvent)
+    {
+        _endEvent = endEvent;
+
+    }
+
+    void AnimationEnd()
+    {
+        Debug.Log("AnimationEnd");
+        if(null != _endEvent) // 함수변수 
+        {
+            _endEvent(); //우리가 등록한 함수
+        }
+    }
+
+
+}
--- Play ch/Assets/01. Scripts/Character.cs
+++ Play ch/Assets/01. Scripts/Character.cs
@@ -4,26 +4,87 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+
+
     //SerializeField 
-    //CharacterControllor
-    [SerializeField] Animator _animator;    
+    //CharacterControllor를 _animator에 세팅
+    
+    [SerializeField] AnimationController _animationController;
+
+    enum eState
+    {
+        IDLE,
+        WAIT,
+        KICK,
+    }
 
     void Start()
     {
-        IdleState();
+        //IdleState();
+        //WaitState();
+        ChangeState(eState.IDLE); //()=>{} 무기명함수
+        /*
+        _animationController.AddEndEvent(()=>
+        {
+            Debug.Log("Animation Test");
+            // Idle -> wait
+            // wait -> kick
+            // kick -> Idel
+        });
+        */
     }
 
     void Update()
     {
     }
 
-        //상태 
+    void ChangeState(eState state)
+    {
+        switch (state)
+        {
+            case eState.IDLE:
+                IdleState();
+                break;
+            case eState.WAIT:
+                WaitState();
+                break;
+            case eState.KICK:
+                KickState();
+                break;
+
+
+        }
+    }
+
+    //상태 x zz
     void IdleState()
     {
-        string trggerName = "idle1";
+        /*string trggerName = "idle1";
         int randValue = Random.Range(1, 6);
         trggerName = "idle" + randValue;
-        _animator.SetTrigger(trggerName);
-        
+        _animator.SetTrigger(trggerName); */
+        //Invoke("WaitState",2); //2초후에 WaitState 가장 기본적인 안좋은
+        _animationController.PLay("idle1", () =>
+        {
+            WaitState();
+        });
+     }
+
+    void WaitState()
+    {
+        _animationController.PLay("idle2", () =>
+        {
+            KickState();
+        });
+        //Invoke("IdleState", 2); //확인
     }
+
+    void KickState()
+    {
+        _animationController.PLay("idle5", () =>
+        {
+            IdleState();
+        });
+    }
+
 }
